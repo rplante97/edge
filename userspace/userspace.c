3e9e7fd24a7cdd1a1f6062af00ab2355c133ad50
Ryan Plante <ryan.plante@maine.edu>
2019-04-18 06:29:32 -0400

    Userspace code now tests locking w/ multiple processes

--- userspace/userspace.c
+++ userspace/userspace.c
@@ -10,6 +10,12 @@ int main(int argc, char *argv[])
 	char * encoder_input;
 	int fd;
 	int input_len;
+	pid_t lock_test;
+	
+	if(argc != 2){
+		printf("Invalid call. Use ./userspace data\n");
+		return -1;
+	}
 
 	encoder_input = argv[1];
 	input_len = strlen(encoder_input);
@@ -21,10 +27,22 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 	
-	if(write(fd, encoder_input, input_len) < 0){
-		printf("Failed to write to device!");
-		return -1;	
+	//Spawn multiple processes to test locking
+	lock_test = fork();
+	
+	if(lock_test > 0){
+		if(write(fd, encoder_input, input_len) < 0){
+			printf("Failed to write to device!");
+			return -1;	
+		}
 	}
+	else {
+		if(write(fd, encoder_input, input_len) < 0){
+			printf("Failed to write to device!");
+			return -1;	
+		}
+	}
+	
 	close(fd);
 	return 0;
 }
